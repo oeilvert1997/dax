eda897f095a88cdd3a2418223adeb22df4eb2ef4
oeilvert <96994538+oeilvert1997@users.noreply.github.com>
2025-03-25 17:14:17 +0900

    Update install.ts

--- install.ts
+++ install.ts
@@ -1,12 +1,12 @@
 #!/usr/bin/env -S deno run --allow-all
 import $ from "@david/dax"; // "dax-sh" in Node
 
-const homeDir = Deno.env.get("HOME");
-const installDir = homeDir + '/.local/bin';
+const homePath = Deno.env.get("HOME");
+const installPath = homePath + '/.local/bin';
 
 // run a command
-await $`mkdir -p ${installDir}`;
-await $`curl -L https://github.com/neovim/neovim/releases/latest/download/nvim-linux-x86_64.tar.gz -o ${homeDir}`;
-await $`rm -rf ${installDir}/nvim`;
-await $`tar -xzf ${homeDir}/nvim-linux-x86_64.tar.gz -C ${installDir}`;
-await $`rm -rf ${homeDir}/nvim-linux-x86_64.tar.gz`;
+await $`mkdir -p ${installPath}`;
+await $`curl -L https://github.com/neovim/neovim/releases/latest/download/nvim-linux-x86_64.tar.gz -o ${homePath}/nvim-linux-x86_64.tar.gz`;
+await $`rm -rf ${installPath}/nvim`;
+await $`tar -xzf ${homeDir}/nvim-linux-x86_64.tar.gz -C ${installPath}`;
+await $`rm -rf ${homePath}/nvim-linux-x86_64.tar.gz`;
