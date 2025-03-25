087b48a6eb80c7338ba1b2a23e18461f81dc2026
oeilvert <96994538+oeilvert1997@users.noreply.github.com>
2025-03-25 09:23:07 +0900

    Update hello.ts

--- hello.ts
+++ hello.ts
@@ -2,7 +2,7 @@
 import $ from "@david/dax"; // "dax-sh" in Node
 
 // run a command
-await $`echo $HOME`
+await $`mkdir -p $HOME/mkdirtest1`
 // await $`curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux-x86_64.tar.gz`;
 // await $`mkdir -p ~/.local/bin`
 // await $`rm -rf ~/.local/bin/nvim`;
