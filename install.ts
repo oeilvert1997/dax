c3fc66488ac86a19d08715ce57f336a657436b94
oeilvert <96994538+oeilvert1997@users.noreply.github.com>
2025-03-25 14:16:14 +0900

    Update and rename hello.ts to install.ts

--- install.ts
+++ install.ts
@@ -1,8 +1,12 @@
 #!/usr/bin/env -S deno run --allow-all
 import $ from "@david/dax"; // "dax-sh" in Node
 
+const homeDir = Deno.env.get("HOME");
+const installDir = homeDir + "/.local/bin;
+
 // run a command
-await $`echo $HOME`;
+await $`echo ${homeDir}`;
+await $`echo ${installDir}`;
 // await $`curl -L https://github.com/neovim/neovim/releases/latest/download/nvim-linux-x86_64.tar.gz -o ${HOME}`;
 // await $`mkdir -p ${HOME}/.local/bin`;
 // await $`rm -rf ${HOME}/.local/bin/nvim`;
