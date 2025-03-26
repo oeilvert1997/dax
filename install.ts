0d690e7451624e4fb1565de52e12ce09652d57fa
oeilvert <96994538+oeilvert1997@users.noreply.github.com>
2025-03-26 13:55:08 +0900

    Update install.ts

--- install.ts
+++ install.ts
@@ -9,5 +9,5 @@ await $`curl -LsS https://github.com/neovim/neovim/releases/latest/download/nvim
 await $`sudo tar -xzf ${homePath}/nvim-linux-x86_64.tar.gz -C ${installPath}`;
 await $`rm -rf ${homePath}/nvim-linux-x86_64.tar.gz`;
 // await $`echo 'export PATH=$HOME/bin:$HOME/.local/bin:/usr/local/bin:$PATH:/opt/nvim-linux-x86_64/bin' >> ${homePath}/.zshrc`;
-await $`echo '${homePath}/.hello.txt'`;
+await $`echo 'homePath is ${homePath}`;
 await $`echo 'export PATH=$HOME/bin:$HOME/.local/bin:/usr/local/bin:$PATH:/opt/nvim-linux-x86_64/bin' >> ${homePath}/hello.txt`;
