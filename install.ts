a707883112e5677f5edefbf68ba2eb2f4fee46a1
oeilvert <96994538+oeilvert1997@users.noreply.github.com>
2025-03-26 17:34:19 +0900

    Update install.ts

--- install.ts
+++ install.ts
@@ -6,6 +6,6 @@ import $ from "@david/dax"; // "dax-sh" in Node
 
 // run a command
 await $`curl -LsS https://github.com/neovim/neovim/releases/latest/download/nvim-linux-x86_64.tar.gz -o $HOME/nvim-linux-x86_64.tar.gz`;
-await $`sudo tar -xzf $HOME/nvim-linux-x86_64.tar.gz -C $/opt`;
+await $`sudo tar -xzf $HOME/nvim-linux-x86_64.tar.gz -C /opt`;
 await $`rm -rf $HOME/nvim-linux-x86_64.tar.gz`;
 await $`echo 'export PATH=$HOME/bin:$HOME/.local/bin:/usr/local/bin:$PATH:/opt/nvim-linux-x86_64/bin' >> $HOME/.zshrc`;
