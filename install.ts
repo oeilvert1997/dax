88113944c40975ea7e2ae99b7d11544aa13eb000
oeilvert <96994538+oeilvert1997@users.noreply.github.com>
2025-03-26 10:45:58 +0900

    Update install.ts

--- install.ts
+++ install.ts
@@ -2,11 +2,9 @@
 import $ from "@david/dax"; // "dax-sh" in Node
 
 const homePath = Deno.env.get("HOME");
-const installPath = homePath + '/.local/bin';
+const installPath = '/opt';
 
 // run a command
-await $`mkdir -p ${installPath}`;
 await $`curl -LsS https://github.com/neovim/neovim/releases/latest/download/nvim-linux-x86_64.tar.gz -o ${homePath}/nvim-linux-x86_64.tar.gz`;
-await $`rm -rf ${installPath}/nvim`;
-await $`tar -xzf ${homePath}/nvim-linux-x86_64.tar.gz -C ${installPath}`;
+await $`sudo tar -xzf ${homePath}/nvim-linux-x86_64.tar.gz -C ${installPath}`;
 await $`rm -rf ${homePath}/nvim-linux-x86_64.tar.gz`;
